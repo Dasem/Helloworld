9fe889b55dcde6c759a156f05d1f3c413900eb93
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-07-30 13:56:41 +0500

    change on main branch

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,7 +1,13 @@
 package com.example.helloworld;
 
 public class HelloWorld {
+
     public static void main(String[] args){
         System.out.println("Hello World");
+        something();
+    }
+
+    public static void something(){
+        System.out.println("Something");
     }
 }
