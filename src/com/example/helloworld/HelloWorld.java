17192d09ee4c1688ea9c5a714616cf1cb52b3dc4
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-07-30 16:33:30 +0500

    1

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -5,9 +5,12 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
         fun1();
+        fun2();
     }
 
     private static void fun1(){
         System.out.println("text 1");
+    }
+    private static void fun2(){
     }
 }
